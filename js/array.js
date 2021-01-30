4f9a88b3601af30736a954b690027af7f1e38190
notabu <samyyklass@gmail.com>
2021-01-30 12:19:54 +0600

    Добавил решение задачи темы: Синтаксис

--- js/array.js
+++ js/array.js
@@ -1,2 +1,14 @@
 // Задачи из JS: Массивы / hexlet
 
+// Синтаксис
+/*
+const getWeekends = (str = 'default') => {
+  if (str === 'long' || str === 'default') {
+    return ['saturday', 'sunday'];
+  }
+
+  return ['sat', 'sun'];
+}
+
+console.log(getWeekends());
+*/
