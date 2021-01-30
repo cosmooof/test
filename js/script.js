a222dd16cec2df16c2ee1d345fe510d5969dae17
notabu <samyyklass@gmail.com>
2021-01-30 14:00:05 +0600

    Обновил список

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// const pi = 3.14;
-// const R = 5;
-// const superR = R ** 2;
-//
-// const area = pi * superR * 4;
-// console.log(area);
 
 /*
 * Основы программирования
@@ -60,21 +54,143 @@ export default getSumArea; // export default module
 
 
 
-const smallestDivisor = (num) => {
+// const smallestDivisor = (num) => {
+//
+//     const iter = (current, acc) => {
+//       if ((current / acc) % 1) {
+//         return acc;
+//       }
+//
+//       return acc++;
+//     }
+//
+//     return acc;
+// }
 
-    const iter = (num) = {
-        let acc = 2;
-
-        if (num === 1) {
+/*const smallestDivisor = (num) => {
+    const iter = (acc) => {
+        if (acc > num / 2) {
             return num;
-        } else if ((num / acc) === 1) {
-            return num;
+        } else if (num % acc === 0) {
+            return acc;
         }
+
+        return iter(acc + 1);
     }
 
-    return smallestDivisor(num % 2);
+    return iter(2);
 }
 
-console.log(smallestDivisor(15));
+console.log(smallestDivisor(15));*/
 
 
+
+
+
+// const NOD = (a, b) => {
+//     if (b === 0) {
+//         return a;
+//     }
+//
+//     return NOD(b, a % b);
+// }
+//
+// console.log(NOD(6, 3));
+
+// isPrime - функция для нахождения простого натурального числа
+/*const isPrime = (x) => {
+    if (x < 2) {
+        return false;
+    }
+
+    let i = 2;
+    while (i < x) {
+        if (x % i === 0) {
+            return false;
+        }
+
+        i++;
+    }
+
+    return true;
+};
+
+console.log(isPrime(9));*/
+
+// функция которая считает сумму всех натуральных чисел
+// const sum = (n, a, b) => {
+//     let result = 0;
+//
+//     for (let i = 1; i < n; i++) {
+//         if (i % a === 0 || i % b === 0) {
+//             result += i;
+//         }
+//     }
+//
+//     return result;
+// }
+
+
+
+// console.log(9 % 1);
+
+// 2 4 6 8
+// 4 8
+
+// 1 2 3 4 5 6 7 8 9
+
+/*
+const reverse = (str) => {
+    let result = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    };
+
+    return result;
+}
+*/
+
+// const calc = (op, one, two) => {
+//     switch (op) {
+//         case '+':
+//             return one + two;
+//         case '-':
+//             return one - two;
+//         case '/':
+//             return one / two;
+//         case '*':
+//             return one * two;
+//             break;
+//
+//         default:
+//             return NaN;
+//     }
+// }
+
+//
+// const addDigits = (num) => {
+//     const char = String(num); // 2
+//     let i = 0;
+//     let result = 0;
+//
+//     while (String(result.length) != 0) {
+//         result += Number(char[i]);
+//         i++;
+//
+//         // for (let i = 0; i < char.length; i++) {
+//         //     result = result + String(char[i]);
+//         // }
+//     }
+//
+//     return result;
+//
+//     // for (let i = 0; i < char.length; i++) {
+//     //     const sum = char[i];
+//     //     // result = result + String(char[i]);
+//     // }
+//
+//     // return result;
+// }
+//
+// console.log(addDigits(11));
