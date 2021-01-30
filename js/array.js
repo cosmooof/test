0d73eda869e2f9000b1767b27ab43010826fddfc
notabu <samyyklass@gmail.com>
2021-01-30 12:56:33 +0600

    Добавил решение задачи темы: Модификация

--- js/array.js
+++ js/array.js
@@ -12,3 +12,20 @@ const getWeekends = (str = 'default') => {
 
 console.log(getWeekends());
 */
+
+// Модификация
+/*
+const swap = (items) => {
+  if (items.length < 2) {
+    return items;
+  }
+  const lastIndex = items.length - 1;
+  const last = items[lastIndex];
+  items[lastIndex] = items[0];
+  items[0] = last;
+
+  return items;
+};
+
+console.log(swap(['one', 'two', 'three']));
+*/
