72e8f5fac2974fec198cef6e7c6884144e5ef30d
notabu <samyyklass@gmail.com>
2021-02-01 07:01:23 +0600

    Добавил решение задачи темы: Агрегация

--- js/array.js
+++ js/array.js
@@ -73,3 +73,23 @@ const reverse = (arr) => {
     arr.push(tempArr[i]);
   }
 }*/
+
+// Агрегация
+/*const coll1 = [2, 0, 17, 3, 9, 15, 4];
+
+const calculateSum = (arr) => {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] % 3 === 0) {
+      sum += arr[i];
+    }
+  }
+
+  return sum;
+}
+
+console.log(calculateSum(coll1));*/
