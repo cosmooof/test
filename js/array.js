d91a8f2ec69fb38bdd6b1c1c00be81c361b9ed4f
notabu <samyyklass@gmail.com>
2021-02-03 00:43:09 +0600

    Добавил решение задачи темы: Управляющие инструкции / Обновил

--- js/array.js
+++ js/array.js
@@ -149,3 +149,21 @@ const getTotalAmount = (arr, name) => {
   return sum;
 }
 */
+
+// Вложенные массивы
+/*const getSuperSeriesWinner = (scores) => {
+  let result = 0;
+  for (const score of scores) {
+    result += Math.sin(score[0] - score[1]);
+  }
+
+  if (result > 0) {
+    return 'canada';
+  }
+
+  if (result < 0) {
+    return 'ussr';
+  }
+
+  return null;
+}*/
