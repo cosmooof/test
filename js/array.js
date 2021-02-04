49ad8ecc0d115e07c8cbe3e9473d1abeb5a5d7b4
notabu <samyyklass@gmail.com>
2021-02-05 04:02:02 +0600

    Добавил решение задачи темы: Сортировка массивов ? Алгорит: Сортировка пузырьком

--- js/array.js
+++ js/array.js
@@ -234,3 +234,26 @@ const countUniqChars = (text) => {
 
   return uniqChars.length;
 };*/
+
+// Сортировка массивов
+/*const bubbleSort = (coll) => {
+  let stepsCount = coll.length - 1;
+  let swapped;
+
+  do {
+    swapped = false;
+
+    for (let i = 0; i < stepsCount; i += 1) {
+      if (coll[i] > coll[i + 1]) {
+        const temp = coll[i];
+        coll[i] = coll[i + 1];
+        coll[i + 1] = temp;
+
+        swapped = true;
+      }
+    }
+    stepsCount -= 1;
+  } while (swapped);
+
+  return coll;
+}*/
