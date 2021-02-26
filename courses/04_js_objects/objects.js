ffca99067be8a1bda0dfaf8c7b089d55dc78a74b
notabu <samyyklass@gmail.com>
2021-02-26 15:09:43 +0600

    добавил решение задачи - Модификация

--- courses/04_js_objects/objects.js
+++ courses/04_js_objects/objects.js
@@ -1,10 +1,16 @@
 // Синтаксис
-/*
-export default () => {
+/*export default () => {
   const obj = {
     files: ['src/objects.js'],
     config: true,
   };
   return obj;
-};
-*/
+};*/
+
+// Модификация
+/*const normalize = (obj) => {
+  obj.name = _.capitalize(obj.name);
+  obj.description = obj.description.toLowerCase();
+};*/
+
+
