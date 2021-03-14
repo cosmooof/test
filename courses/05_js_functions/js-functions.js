d8eac2f630186c9fa9da90a3c310f5b504480d10
notabu <samyyklass@gmail.com>
2021-03-14 06:48:04 +0600

    add rest-operator

--- courses/05_js_functions/js-functions.js
+++ courses/05_js_functions/js-functions.js
@@ -19,3 +19,14 @@ const sayPrimeOrNot = (num) => {
   const text = isPrime(num) ? 'yes' : 'no';
   console.log(text);
 }*/
+// Оператор Rest (упаковка аргументов)
+/*
+const average = (...num) => {
+  const count = num.length;
+
+  if (count === 0) {
+    return null;
+  }
+
+  return _.sum(num) / count;
+}*/
