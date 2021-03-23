524d0af454eed6b2851a5571c8c6186f50b88760
notabu <samyyklass@gmail.com>
2021-03-23 13:33:18 +0600

    add goal

---
+++ courses/06_js_testing/testing.js
@@ -0,0 +1,11 @@
+// Зачем и как писать тесты?
+/*
+if (get({ kiln: 'oil'}, 'kiln') !== 'oil') {
+  throw new Error('Функция работает неверно!');
+}
+if (get({}, 'kiln', 'air') !== 'air') {
+  throw new Error('Функция работает неверно!');
+}
+if (get({ kiln: 'oil'}, 'kiln', 'air') !== 'oil') {
+  throw new Error('Функция работает неверно!');
+}*/
