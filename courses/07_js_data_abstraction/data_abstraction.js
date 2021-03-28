4f63c6f6f9a8fa8399baf5dd86311e6708e4b21a
notabu <samyyklass@gmail.com>
2021-03-28 10:56:09 +0600

    add arrays

--- courses/07_js_data_abstraction/data_abstraction.js
+++ courses/07_js_data_abstraction/data_abstraction.js
@@ -5,3 +5,10 @@
 
   return Math.sqrt((deltaX ** 2) + (deltaY ** 2));
 };*/
+// Семантика массивов
+/*const getMidpoint = (point1, point2) => {
+  const x = (point1.x + point2.x) / 2;
+  const y = (point1.y + point2.y) / 2;
+
+  return { x, y };
+}*/
