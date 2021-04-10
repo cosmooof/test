2b066f085d4f7204efcffc83c41de95c561acf63
notabu <samyyklass@gmail.com>
2021-04-10 16:59:48 +0600

    add manipulations

--- courses/08_js_trees/trees.js
+++ courses/08_js_trees/trees.js
@@ -24,3 +24,19 @@ const removeFirstLevel = (tree) => {
     ], { owner: 'root', hidden: false }),
   ], { hidden: true });
 };*/
+// Манипуляции с виртуальной файловой системой
+/*export const compressImages = (node) => {
+  const children = getChildren(node);
+  const newChildren = children.map((child) => {
+    const name = getName(child);
+    if (!isFile(child) || !name.endsWith('.jpg')) {
+      return child;
+    }
+    const meta = getMeta(child);
+    const newMeta = _.cloneDeep(meta);
+    newMeta.size /= 2;
+    return mkfile(name, newMeta);
+  });
+  const newMeta = _.cloneDeep(getMeta(node));
+  return mkdir(getName(node), newChildren, newMeta);
+};*/
