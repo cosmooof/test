a2cbc322f5c3ebde628c88a06fbda9a8f769b3c0
notabu <samyyklass@gmail.com>
2021-04-13 16:28:48 +0600

    add aggregation

--- courses/08_js_trees/trees.js
+++ courses/08_js_trees/trees.js
@@ -54,3 +54,16 @@ const downcaseFileNames = (node) => {
 };
 
 export default downcaseFileNames; */
+// Агрегация
+/* const getHiddenFilesCount = (node) => {
+  const name = getName(node);
+  if (isFile(node)) {
+    return name.startsWith('.') ? 1 : 0;
+  }
+
+  const children = getChildren(node);
+  const hiddenFilesCounts = children.map(getHiddenFilesCount);
+  return _.sum(hiddenFilesCounts);
+};
+
+export default getHiddenFilesCount; */
